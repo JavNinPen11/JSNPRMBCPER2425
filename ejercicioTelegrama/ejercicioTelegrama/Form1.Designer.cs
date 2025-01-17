cddf9c497e4f81cb9439b23b26534d19b12ec91c
RAQUEL MARIA BLÁZQUEZ CORRAL <tanistdkj@gmail.com>
2025-01-17 20:05:42 +0100

    raquel

--- ejercicioTelegrama/ejercicioTelegrama/Form1.Designer.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Form1.Designer.cs
@@ -28,31 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            btnCalcularPrecio = new Button();
+            chkUrgente = new CheckBox();
+            txtTelegrama = new RichTextBox();
+            txtPrecio = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
-            // button1
+            // btnCalcularPrecio
             // 
-            button1.Location = new Point(249, 156);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnCalcularPrecio.Location = new Point(263, 191);
+            btnCalcularPrecio.Margin = new Padding(3, 2, 3, 2);
+            btnCalcularPrecio.Name = "btnCalcularPrecio";
+            btnCalcularPrecio.Size = new Size(82, 22);
+            btnCalcularPrecio.TabIndex = 0;
+            btnCalcularPrecio.Text = "Calcular precio";
+            btnCalcularPrecio.UseVisualStyleBackColor = true;
+            btnCalcularPrecio.Click += btnCalcularPrecio_Click;
+            // 
+            // chkUrgente
+            // 
+            chkUrgente.AutoSize = true;
+            chkUrgente.Location = new Point(84, 163);
+            chkUrgente.Name = "chkUrgente";
+            chkUrgente.Size = new Size(68, 19);
+            chkUrgente.TabIndex = 1;
+            chkUrgente.Text = "Urgente";
+            chkUrgente.UseVisualStyleBackColor = true;
+            // 
+            // txtTelegrama
+            // 
+            txtTelegrama.Location = new Point(75, 41);
+            txtTelegrama.Name = "txtTelegrama";
+            txtTelegrama.Size = new Size(270, 116);
+            txtTelegrama.TabIndex = 2;
+            txtTelegrama.Text = "";
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(144, 191);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(100, 23);
+            txtPrecio.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(78, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(35, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Texto";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(79, 195);
+            label2.Name = "label2";
+            label2.Size = new Size(37, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Coste";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            ClientSize = new Size(441, 265);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPrecio);
+            Controls.Add(txtTelegrama);
+            Controls.Add(chkUrgente);
+            Controls.Add(btnCalcularPrecio);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private Button btnCalcularPrecio;
+        private CheckBox chkUrgente;
+        private RichTextBox txtTelegrama;
+        private TextBox txtPrecio;
+        private Label label1;
+        private Label label2;
     }
 }
