ba621c4b490927402b1c9bfa806f927733bf5875
peufee <paulaescri24@gmail.com>
2025-01-27 17:41:51 +0100

    Cambios en precios de los telegramas

--- ejercicioTelegrama/ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Form1.cs
@@ -39,7 +39,7 @@ namespace ejercicioTelegrama
                 {
                     if (numPalabras <= 10)
                     {
-                        coste = 2.5;
+                        coste = 3;
                     }
                     else
                     {
@@ -50,7 +50,7 @@ namespace ejercicioTelegrama
                 {
                     if (numPalabras <= 10)
                     {
-                        coste = 5;
+                        coste = 6;
                     }
                     else
                     {
